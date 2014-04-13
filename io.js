d1dd9596c39482b2c7363a6ce4f7b2a393e35543
Charles Julian Knight <charles@rabidaudio.com>
2014-04-12 23:05:16 -0400

    device picking and refactor

--- io.js
+++ io.js
@@ -1,6 +1,8 @@
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
 var events = require("events");
+var underscore = require('underscore');
+var fs = require('fs');
 
 var keymap = {
 	'0': "play_pause",
@@ -13,24 +15,18 @@ var keymap = {
 var commandmap = {
 	'display': 0,
 }
-var serial_dir = "/dev/ttyACM0";
-//_.filter(fs.readdirSync('/dev'), function(device){ return device.match(/^ttyACM/); });
+//var serial_dir = "/dev/ttyACM0";
+
+//is it elegant? no. functional? yes.
+var serial_dir = underscore.filter(fs.readdirSync('/dev'), function(device){ return device.match(/^ttyACM/); });
 
 function IO(){
 	events.EventEmitter.call(this);
-	serialPort = new SerialPort(serial_dir, {
+	if(serial_dir.length < 1) throw "No serial devices found.";
+	serialPort = new SerialPort("/dev/"+serial_dir.shift(), {
 	  baudrate: 57600,
 	  parser: serialport.parsers.readline("\n")
 	}, false);
-
-	var that = this;
-	serialPort.open(function(err){
-		if(err) throw err;
-		serialPort.on('data', function(data){
-			console.log("data recvd");
-			that.emit(keymap[data]);
-		});
-	});
 	this.serialPort = serialPort;
 }
 
@@ -46,5 +42,23 @@ IO.prototype.send = function(command, message){
 	this.serialPort.write(message);
 }
 
+IO.prototype.open = function(cb){
+	var that = this;
+	serialPort.open(function(err){
+		if(err){
+			console.log(err);
+			console.log("trying next device...");
+			that = new IO(); //try the next found device
+			that.open(cb);
+		}else{
+			serialPort.on('data', function(data){
+				console.log("data recvd");
+				that.emit(keymap[data]);
+			});
+			cb(that);
+		}
+	});
+}
 
-module.exports = new IO();
+
+module.exports = new IO(); //sneaky singleton awesomeness
--- main.js
+++ main.js
@@ -14,33 +14,35 @@ console.log("Setting up...");
 
 //var playlist;
 
+function main(io){
+	find.file(/\.mp3$/, library_dir, function(files){
 
-find.file(/\.mp3$/, library_dir, function(files){
+		shuffle(files);
+		for(var i=0;i<files.length;i++){
+			files[i]=fs.realpathSync(files[i]);
+		}
 
-	shuffle(files);
-	for(var i=0;i<files.length;i++){
-		files[i]=fs.realpathSync(files[i]);
-	}
+		playlist = new Playlist(files);
+		playlist.on("play", function(tags){
+			console.log("Now playing " + tags.title);
+			io.send('display', tags.artist + "\t" + tags.title + " - " + tags.album);
+		});
 
-	playlist = new Playlist(files);
-	playlist.on("play", function(tags){
-		console.log("Now playing " + tags.title);
-		io.send('display', tags.artist + "\t" + tags.title + " - " + tags.album);
+		var next = playlist.next.bind(playlist);
+		var prev = playlist.prev.bind(playlist);
+		var pause = playlist.pause.bind(playlist);
+		var play = playlist.play.bind(playlist);
+
+		io.on('prev', prev);
+		io.on('next', next);
+		//io.on('prev', pause);
+		//io.on('next', play);
+
+		playlist.play();
 	});
+}
 
-	var next = playlist.next.bind(playlist);
-	var prev = playlist.prev.bind(playlist);
-	var pause = playlist.pause.bind(playlist);
-	var play = playlist.play.bind(playlist);
-
-	//io.on('prev', prev);
-	//io.on('next', next);
-	io.on('prev', pause);
-	io.on('next', play);
-
-	playlist.play();
-});
-
+io.open(main);
 
 //testing purposes 
 require("repl").start({
