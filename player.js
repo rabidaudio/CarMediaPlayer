5e9500419a878addec7942b3d725ad146163ed01
Charles Julian Knight <charles@rabidaudio.com>
2014-05-22 16:56:09 -0400

    Removed old timer stuff, refactored state code

--- player.js
+++ player.js
@@ -1,49 +1,53 @@
 var fs      = require('fs');
-var events  = require("events");
+var events  = require('events');
 var lame    = require('lame');
 var Speaker = require('speaker');
 var through = require('through');
 
 var tags    = require('./tags');
 
-var time_interval = 50;
 
-function create_speaker() {
-  return new Speaker({
-    channels   : 2,     // 2 channels
-    bitDepth   : 16,    // 16-bit samples
-    sampleRate : 44100  // 44,100 Hz sample rate
-  });
-}
+// All of these are valid properties, values of Player.state and also events
+var states = [
+  'unstarted',
+  'playing',
+  'paused',
+  'stopped',
+  'ended',
+];
+
+// Settings for new Speaker objects
+var speaker_options = {
+  channels   : 2,     // 2 channels
+  bitDepth   : 16,    // 16-bit samples
+  sampleRate : 44100  // 44,100 Hz sample rate
+};
 
 function Player(file) {
   if (!file) { return; }
   this.file    = file;
-  this.speaker = create_speaker();
+  this.speaker = new Speaker(speaker_options);
   this.decoder = new lame.Decoder();
 
   this.buffer = fs.createReadStream(file);
 
-  var that = this;
+  var self = this;
   this.t = through(function (data) {
-    that.seek_position += data.length;
-    //console.log(that.seek_position);
     this.queue(data);
   }, function () {
-    that.emit("end");
+    self._set_state('ended');
     this.queue(null);
   });
 
-  this.playing       = false;
-  this.paused        = false;
-  this.seek_position = 0; //bytes
-  this.time          = 0; //ms
-  this.timer         = undefined;
+  //inital state
+  this._set_state('unstarted');
 
   this.tags = tags(file); //TODO get from database instead
+
   events.EventEmitter.call(this);
 }
 
+// Normal event object code
 Player.super_ = events.EventEmitter;
 Player.prototype = Object.create(events.EventEmitter.prototype, {
   constructor: {
@@ -52,39 +56,51 @@ Player.prototype = Object.create(events.EventEmitter.prototype, {
   }
 });
 
+// Used to update the state of the Player. Valid states are
+//  in the global state array. Also emits events. Array.indexOf
+//  is a bit inefficient...
+Player.prototype._set_state = function (state) {
+  var s;
+  var state_number = states.indexOf(state);
+  if (state_number < 0) {
+    throw new Error("Player set to invalid state " + state);
+  }
+  for (s in states) {
+    this[states[s]] = false;
+  }
+  this[state] = true;
+  this.state = state;
+  this.emit(state, this.tags);
+  return state_number;
+};
+
 Player.prototype.play = function () {
   if (this.playing) { return true; }
   if (this.paused) {
+    //Reconnect the speaker to the decoder
     this.decoder.pipe(this.speaker);
-    this.timer   = setInterval(this.time_step.bind(this), time_interval);
-    this.paused  = false;
-    this.playing = true;
+    this._set_state('playing');
   } else {
+    //Connect the chain together
     this.buffer.pipe(this.t).pipe(this.decoder).pipe(this.speaker);
-    this.timer   = setInterval(this.time_step.bind(this), time_interval);
-    this.playing = true;
+    this._set_state('playing');
   }
-  this.emit("play", this.tags);
 };
 Player.prototype.stop = function () {
   if (!this.playing) { return false; }
   this.speaker.end();
-  clearInterval(this.timer);
-  this.emit("stop");
+  this._set_state('stopped');
 };
 Player.prototype.pause = function () {
-  console.log("pause called");
   if (this.paused || !this.playing) { return false; }
   this.speaker.end();
-  clearInterval(this.timer);
-  //make a new one so we can continue later
-  this.speaker = create_speaker();
-  this.paused  = true;
-  this.playing = false;
-  this.emit("pause");
-};
+  // make a new one so we can continue later
+  this.speaker = new Speaker(speaker_options);
 
-Player.prototype.play_pause = function() {
+  this._set_state('paused');
+};
+// Helper method for play and pause
+Player.prototype.play_pause = function () {
   if (this.paused || !this.playing) {
     this.play();
   } else {
@@ -92,9 +108,14 @@ Player.prototype.play_pause = function() {
   }
 };
 
-Player.prototype.time_step = function () {
-  this.time += time_interval;
-  //console.log(this.time);
-};
-
 module.exports = Player;
+
+
+/* Test Code */
+p = new Player('Test/chirp.mp3');
+P = Player;
+// p.play();
+
+require("repl").start({
+  useGlobal: true
+});
